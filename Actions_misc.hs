746eb9e0bdae25734f33ed0a5ae84bf8a9329199
Jason To Sing Hin <sht2@st-andrews.ac.uk>
2022-02-08 14:01:12 +0000

    Update Actions_misc.hs
    - minor improvements & commenting

--- Actions_misc.hs
+++ Actions_misc.hs
@@ -7,7 +7,6 @@ import Parsing
 import System.Console.Haskeline
 import Control.Monad.IO.Class
 
-
 {- Converts an Operation to the resulting GameData after 
    applying the Operation and a message for the user through intermediary functions. -}
 operations :: MonadIO m => GameData -> Operation -> InputT m (GameData, String)
@@ -81,13 +80,15 @@ rooms x = case x of
             "street" -> Just street
             _ -> Nothing
 
+savePath = ".\\save\\"
+
 {- Saves the game. -}
 save :: GameData -> String -> IO ()
-save gd fname = writeFile path content
-   where path = ".\\save\\" ++ fname
-         content = init (saveGame gd ++
+save gd fname = writeFile (savePath ++ fname) content
+   where content = init (saveGame gd ++ --'init' removes the trailing '\n'
                          saveRoom (map snd $ world gd))
 
+{- Saves the current GameData. -}
 saveGame :: GameData -> [Char]
 saveGame gd = location_id gd ++ " " ++
               tupleToString (world gd) ++ " " ++
@@ -97,29 +98,37 @@ saveGame gd = location_id gd ++ " " ++
               show (opened gd) ++ " " ++
               show (finished gd) ++ "\n"
 
+{- Saves the current room (objects only).
+   The object name is parsed in a more complicated way
+   not to use the predefined show instance as the object parser
+   does not recognise the full name of the objects. -}
 saveRoom :: [Room] -> [Char]
 saveRoom (x:xs) = show (map obj_name $ objects x) ++ "\n" ++
                   saveRoom xs
 saveRoom [] = []
 
+{- Converts a tuple containing a String and a Room to a String.
+   (Used to save the "world" in GameData to a save file) -}
+tupleToString :: [(String, Room)] -> String
+tupleToString xs = "[" ++ init (foldr (\x rest -> x ++ "," ++ rest) [] [fst elem | elem <- xs]) ++ "]"
+
 {- Loads a game from a save file. -}
-load fname = do content <- readFile path
+load :: [Char] -> IO GameData
+load fname = do content <- readFile $ savePath ++ fname
                 return $ loadGame $ split '\n' content
-   where path = ".\\save\\" ++ fname
 
-
-
+{- Loads the GameData and all Room data. -}
 loadGame :: [String] -> GameData
 loadGame dat = gd5
-   where list = [(fst x,y) | x <- world base | y <- tail dat]
-         xs = split ' ' $ head dat
-         gd0 = loadRoom base (head list)
+   where list = [(fst x,y) | x <- world base | y <- tail dat] --Room data and corresponding room ID
+         xs = split ' ' $ head dat --The GameData in text format
+         gd0 = loadRoom base (head list) --software pipelining
          gd1 = loadRoom gd0 (list !! 1)
          gd2 = loadRoom gd1 (list !! 2)
          gd3 = loadRoom gd2 (list !! 3)
          gd4 = loadRoom gd3 (list !! 4)
-         gd5 = loadRoom gd4 (list !! 5)
-         base = GameData (head xs)
+         gd5 = loadRoom gd4 (list !! 5) --Final room
+         base = GameData (head xs) --The GameData inside the file
                 (stringToTuple $ xs !! 1)
                 (stringToList $ xs !! 2)
                 (stringToBool $ xs !! 3)
@@ -127,43 +136,33 @@ loadGame dat = gd5
                 (stringToBool $ xs !! 5)
                 (stringToBool $ xs !! 6)
 
+{- Loads a Room data. -}
 loadRoom :: GameData -> (String, String) -> GameData
 loadRoom gd dat = updateRoom gd rmid new
    where new = Room desc exit (stringToList $ snd dat)
          rmid = fst dat
          rm = maybe undefined id (rooms rmid)
-         desc = if rm == hall && opened gd
+         desc = if rm == hall && opened gd --Check if the door is opened
                 then openedhall 
                 else room_desc rm
-         exit = if rm == hall && opened gd
+         exit = if rm == hall && opened gd --Check if the door is opened
                 then exits rm ++ openedexits
                 else exits rm
 
-{- Converts a String to a list of Objects.
-   (Used to load the player's inventory from a save file)
-   Used by: load -}
+{- Converts a String to a list of Objects. -}
 stringToList ::  String -> [Object]
 stringToList xs = if list == [""] then [] else map go list
    where list = split ',' $ init $ tail xs
          go x = maybe undefined id (object $ init $ tail x)
 
-{- Converts a tuple containing a String and a Room to a String.
-   (Used to save the "world" in GameData to a save file)
-   Used by: save -}
-tupleToString :: [(String, Room)] -> String
-tupleToString xs = "[" ++ init (foldr (\x rest -> x ++ "," ++ rest) [] [fst elem | elem <- xs]) ++ "]"
-
 {- Converts a String to a tuple containing a String and a Room.
-   (Used to load the "world" in GameData from a save file)
-   Used by: load -}
+   (Used to load the "world" in GameData from a save file) -}
 stringToTuple :: String -> [(String, Room)]
 stringToTuple xs = map go $ split ',' $ init $ tail xs
    where go x = case rooms x of
                   Just sth -> (x, sth)
                   Nothing -> undefined
 
-{- Converts a String to a Bool.
-   (Used to load various parts of GameData from a save file)
-   Used by: load -}
+{- Converts a String to a Bool. -}
 stringToBool :: String -> Bool
-stringToBool string = string == "True"
+stringToBool str = str == "True"
